7365973a81ec88759c7017960de7e45f456387d6
tomas <tomasoliveira208@gmail.com>
2023-10-02 14:59:51 +0100

    Added getters and setters to Class "Utente"

--- src/Utente.java
+++ src/Utente.java
@@ -43,6 +43,105 @@ public class Utente {
         this.idade = idade;
         this.altura = altura;
         this.peso = peso;
-
     }
+
+    // getNome method
+    /**
+     *
+     * @return
+     */
+    public String getNome() {
+        return nome;
+    }
+
+
+    // getGenero method
+    /**
+     *
+     * @return
+     */
+    public String getGenero() {
+        return genero;
+    }
+
+
+    // getIdade method
+    /**
+     *
+     * @return
+     */
+    public int getIdade() {
+        return idade;
+    }
+
+
+    // getAltura method
+    /**
+     *
+     * @return
+     */
+    public float getAltura() {
+        return altura;
+    }
+
+
+    // getPeso method
+    /**
+     *
+     * @return
+     */
+    public float getPeso() {
+        return peso;
+    }
+
+
+    // setNome method
+    /**
+     *
+     * @param nome
+     */
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+
+    // setGenero method
+    /**
+     *
+     * @param genero
+     */
+    public void setGenero(String genero) {
+        this.genero = genero;
+    }
+
+
+    // setIdade method
+    /**
+     *
+     * @param idade
+     */
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+
+    // setAltura method
+    /**
+     *
+     * @param altura
+     */
+    public void setAltura(float altura) {
+        this.altura = altura;
+    }
+
+
+    // setPeso method
+    /**
+     *
+     * @param peso
+     */
+    public void setPeso(float peso) {
+        this.peso = peso;
+    }
+
 }
